4b069ffd3e341f28ed045f91e04329efa804fa4a
Elvis Ning <ningelvis@gmail.com>
2020-03-04 23:21:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,5 +35,5 @@ end
 def using_insert(array)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   @another_language = "Python"
-  @list_of_programming_languages.insert(@another_language)
+  @list_of_programming_languages.insert(@another_language)[4]
 end

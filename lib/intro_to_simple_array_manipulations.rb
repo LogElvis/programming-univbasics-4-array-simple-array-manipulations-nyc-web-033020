7b9bca65cd38579bfd3977b1bd48088cf3c3ad4e
Elvis Ning <ningelvis@gmail.com>
2020-03-04 23:25:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,7 +33,5 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, element)
-  @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  @another_language = "Python"
-  @list_of_programming_languages.insert(@another_language)[4]
+  array.insert(element)
 end

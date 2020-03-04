d02d942f24ffa87dbd3e53c715981b8333d91690
Elvis Ning <ningelvis@gmail.com>
2020-03-04 23:04:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,5 +13,5 @@ end
 
 def pop_with_args(array)
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  @small_dogs = pop_with_args.pop(@dog_breeds)
+  @dog_breeds.pop(2)
 end

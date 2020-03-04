50cd27cd9e6058fb2efc3fa4b97278f22d02d7a6
Elvis Ning <ningelvis@gmail.com>
2020-03-04 23:17:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def shift_with_args(array)
   @ice_cream_brands.shift(2)
 end
 
-def using_concat(@my_favorite_things, more_favs)
+def using_concat(array, array2)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   @my_favorite_things.concat(more_favs)
